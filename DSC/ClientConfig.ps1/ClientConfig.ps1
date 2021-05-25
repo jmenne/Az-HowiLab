e579f712ee76cbe2cf436fce2dfbd970706eb062
jmenne <jo.menne@arcor.de>
2021-05-25 13:06:33 +0200

    ClientConfig DSC erstellt
    corp\EWS in Remotedesktop User Gruppe des Clients
    DataDisk einrichten

---
+++ DSC/ClientConfig.ps1/ClientConfig.ps1
@@ -0,0 +1,50 @@
+﻿<#
+    .ClientConfig
+    This configuration:
+    * Ensures that group corp\ews is in local group Remote Desktop Users
+    * Initialises and formats Data Disk with driveletter F
+
+    jmenne 5/25/20218
+
+#>
+
+configuration ClientConfig
+{
+    param
+   (
+        [Parameter(Mandatory)]
+        [String]$DomainName
+    )
+
+    Import-DscResource -Module PSDesiredStateConfiguration, xDisk, cDisk
+    
+# Apply configuration
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }     
+    }
+
+    xWaitforDisk Disk2
+    {
+         DiskNumber = 2
+         RetryIntervalSec =$RetryIntervalSec
+         RetryCount = $RetryCount
+    }
+
+    cDiskNoRestart ADDataDisk
+    {
+        DiskNumber = 2
+        DriveLetter = 'F'
+    }
+
+    Group RemoteDesktopUsers
+    {
+        GroupName = 'Remote Desktop Users'
+        Ensure = 'Present'
+        MembersToInclude = "corp\EWS"
+    }
+}
